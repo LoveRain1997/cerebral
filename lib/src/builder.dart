2226a7b900a9e290a1365ad5bc531072f510c17f
haog <haog@felaxy.com>
2018-12-10 01:33:15 +0800

    state builder

--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -10,26 +10,51 @@ TypeChecker _stateFieldTypeChecker = const TypeChecker.fromRuntime(StateField);
 
 class CerebralGenerator extends GeneratorForAnnotation<State> {
   @override
-  Iterable<String> generateForAnnotatedElement(Element element, ConstantReader annotation, BuildStep buildStep) {
-    final codeLines = <String>[];
-    codeLines.add('class _${element.name}CerebralMixin {');
-    if (_stateTypeChecker.firstAnnotationOfExact(element).getField('copyWith').toBoolValue()) {
-      codeLines.addAll(this.generateCopyWith(element, annotation, buildStep));
+  Iterable<String> generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) sync* {
+    if (element is! ClassElement) {
+      throw InvalidGenerationSourceError(
+          'Generator cannot target `${element.name}`.',
+          todo: 'Remove the State annotation from `${element.name}`.',
+          element: element);
     }
-    codeLines.add('}');
-    return codeLines;
+    if (!element.name.startsWith('_')) {
+      throw InvalidGenerationSourceError(
+          'Class `${element.name}` is not private, prefer to leave it as private and use the generated one.',
+          todo: 'Make class `${element.name}` private.',
+          element: element);
+    }
+    final className = element.name.substring(1);
+    yield 'class $className {';
+    yield* generateConstructor(element, className);
+    yield* generateFields(element as ClassElement);
+    yield '}';
   }
 
-  Iterable<String> generateCopyWith(Element element, ConstantReader annotation, BuildStep buildStep) {
-    if (element is! ClassElement) {
+  Iterable<String> generateConstructor(
+      ClassElement element, String className) sync* {
+    yield '$className({';
+    for (FieldElement value
+        in element.fields.where((field) => field.isPublic)) {
+      yield '${value.type} ${value.name},';
     }
-    final codeLines = <String>[];
-    codeLines.add('''
-    static ${element.name} copyWith({
-    
-    }) {return ${element.name}();}
-    ''');
-    return codeLines;
+    yield '}) : ${element.fields.where((field) => field.isPublic).map<String>((field) => 'this._${field.name} = ${field.name}').join(',')};';
+  }
+
+  // TODO add for list and map operator
+  Iterable<String> generateFields(ClassElement element) sync* {
+    for (FieldElement value
+        in element.fields.where((field) => field.isPublic)) {
+      yield '${value.type} _${value.name};';
+    }
+    for (FieldElement value
+        in element.fields.where((field) => field.isPublic)) {
+      yield '${value.type} get ${value.name} => this.${value.name};';
+    }
+    for (FieldElement value
+        in element.fields.where((field) => field.isPublic)) {
+      yield 'set ${value.name}(${value.type} _${value.name}) => this.${value.name} = _${value.name};';
+    }
   }
 }
 
